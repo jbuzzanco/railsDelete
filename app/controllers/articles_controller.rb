dc330780207acf3c9222ce8f1736b6439b0bf29c
jbuzzanco <jcbuzzanco@gmail.com>
2017-08-01 21:15:00 -0400

    Adds the view for index and to view specific articles
    also adds links to and from the articles to go to them and back to the
    index, and to the blog index from the home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,13 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
@@ -13,4 +22,5 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :text)
     end
+
 end
